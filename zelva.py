daff218e0d688210b96b47cfc4f902719294a93e
jonasfrk <jonas.frk.s@gyarab.cz>
2025-03-07 08:53:08 +0100

    zelva

---
+++ zelva.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+
+
+left(90)
+forward(100)  
+
+
+right(45)       
+forward(70)   
+right(90)
+forward(70)     
+left(45)      
+
+exitonclick()  
