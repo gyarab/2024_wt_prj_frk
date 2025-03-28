aa943685f8b00fef76019239f3e6910883f50f3d
jonasfrk <jonas.frk.s@gyarab.cz>
2025-03-28 12:18:34 +0100

    commit

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import GPU
 
 class GPUAdmin(admin.ModelAdmin):
-    list_display = ["name", "generation", "Vram_amount", "release_date", "price"]
+    list_display = ["name", "generation", "Vram_amount_GB", "release_year", "price_USD", "graphics_processor"]
 
 admin.site.register(GPU, GPUAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class GPU(models.Model):
-    name = models.IntegerField(max_length=100, default="")
+    name = models.CharField(max_length=30, default="")
     generation = models.CharField(max_length=4, default="")
-    Vram_amount = models.IntegerField(blank=True, null=True, default="")
-    release_date = models.DateField(max_length=20, default="")
-    price = models.IntegerField(null=False, blank=False, default="")
+    Vram_amount_GB = models.CharField(max_length=20, default="")
+    release_year = models.IntegerField(null=False, blank=False, default="")
+    price_USD = models.IntegerField(null=False, blank=False, default="")
+    graphics_processor = models.CharField(max_length=10, default="")
 
-def _str_(self):
-    return f"{self.name} {self.generation} {self.Vram_amount} {self.release_date} {self.price} "
+
+def __str__(self):
+    return f"{self.name} {self.generation} {self.Vram_amount_GB} {self.release_year} {self.price_USD} {self. graphics_processor} "
