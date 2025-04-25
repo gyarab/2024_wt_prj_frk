4de72ecfe63a7c8c969313613d9f620fc8280fc1
jonasfrk <jonas.frk.s@gyarab.cz>
2025-04-25 12:15:22 +0200

    COMMIT

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from main.models import GPU
 
-# Create your views here.
+def get_homepage(request):
+    context = {
+        "svatek": "Libor",
+        # SELECT * from GPU ORDER BY 'title' LIMIT 20;
+        "GPU": GPU.objects.all().order_by('title')
+    }
+    return render (request, "main/homepage.html", context)
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from main.views import get_homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
