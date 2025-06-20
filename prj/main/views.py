ab01a6ac94814ed77b5e10b1142c4dadc85bbb70
jonasfrk <jonas.frk.s@gyarab.cz>
2025-06-20 12:08:09 +0200

    COMMIT

--- prj/main/views.py
+++ prj/main/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from main.models import GPU
 
 def get_homepage(request):
-    context = {
-        "svatek": "Libor",
+    context = {  
         # SELECT * from GPU ORDER BY 'title' LIMIT 20;
         "GPU": GPU.objects.all().order_by('title')
     }
